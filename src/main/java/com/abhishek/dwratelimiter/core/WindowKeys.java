21c9745d503975c8734f6c0c509b4b961e6212ce
Gunda Abhishek <gunda.abhishek@phonepe.com>
2019-10-07 18:53:48 +0530

    Window Keys added

--- src/main/java/com/abhishek/dwratelimiter/core/WindowKeys.java
+++ src/main/java/com/abhishek/dwratelimiter/core/WindowKeys.java
@@ -1,4 +1,17 @@
 package com.abhishek.dwratelimiter.core;
 
 public class WindowKeys {
+    private long buckets;
+    private long bucketid;
+    private long bucket_start;
+    private String counter;
+    private String currentTS;
+
+    public WindowKeys(long time, int duration , int precision){
+        this.buckets = (long) Math.ceil(duration/(double)precision);
+        this.bucketid = (long) (time/precision);
+        this.bucket_start = bucketid - buckets +1;
+        this.counter = "" + duration + ":" + precision +":";
+        this.currentTS = counter + "ts";
+    }
 }
