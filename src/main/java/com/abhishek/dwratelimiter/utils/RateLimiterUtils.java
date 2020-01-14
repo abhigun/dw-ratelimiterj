9a24e0e992d6870eb5936e399716138cbc8042c7
abhigun <abhigun@iitk.ac.in>
2020-01-14 11:25:29 +0530

    Minor changes

--- src/main/java/com/abhishek/dwratelimiter/utils/RateLimiterUtils.java
+++ src/main/java/com/abhishek/dwratelimiter/utils/RateLimiterUtils.java
@@ -3,6 +3,7 @@ package com.abhishek.dwratelimiter.utils;
 public class RateLimiterUtils {
 
     private static final long INIT_TIME = (Long.getLong(System.getProperty("time"))/1000);
+
     /*
     Returns the Time counter of the application server from the above initiated time
     Added to avoid the Bin name limitation of 15 characters in Aerospike
